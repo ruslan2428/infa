fc0a5ab1f7fd86a64422eebd7775ca58c33b2be4
rabs00 <131666461+rabs00@users.noreply.github.com>
2025-02-24 18:00:04 +0300

    Update astandgal.cpp

--- astandgal.cpp
+++ astandgal.cpp
@@ -1,26 +1,8 @@
 #include "astandgal.h"
 #include <iostream>
 
-Planet::Planet(const std::string& planetName) : name(planetName) {}
-
-std::string Planet::getName() const {
-    return name;
-}
-
 void Planet::impactByAsteroid(const std::string& asteroidName) const {
     std::cout << "Planet " << name << " has been impacted by asteroid " << asteroidName << "!" << std::endl;
-}
-
-Star::Star(const std::string& starName) : name(starName) {}
-
-std::string Star::getName() const {
-    return name;
-}
-
-Spaceship::Spaceship(const std::string& shipName) : name(shipName) {}
-
-std::string Spaceship::getName() const {
-    return name;
 }
 
 void Spaceship::exploreGalaxy(const Galaxy& galaxy) const {
@@ -44,8 +26,3 @@ void Galaxy::receiveAsteroid(const Asteroid& asteroid) {
     std::cout << "Galaxy " << name << " has received asteroid " << asteroid.getName() << "!" << std::endl;
 }
 
-Spaceport::Rocket::Rocket(const std::string& rocketName) : name(rocketName) {}
-
-void Spaceport::Rocket::launchToPlanet(const Planet& planet) const {
-    std::cout << "Rocket " << name << " arrived on the planet " << planet.getName() << "!" << std::endl;
-}
