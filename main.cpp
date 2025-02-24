4bd337d4028c1e4f4538738a0584fc5b6e38c4c8
rabs00 <131666461+rabs00@users.noreply.github.com>
2025-02-24 17:57:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
-#include "space.h"
+#include "astandgal.h"
 
 int main() {
+    Planet earth("Earth");
+    Star sun("Sun");
+    Spaceship voyager("Voyager");
+    Asteroid apophis("Apophis");
+    Galaxy milkyWay("Milky Way");
 
-    Planet earth("Jupiter");
-    Planet mars("Mars");
+    voyager.exploreGalaxy(milkyWay); // spaceship explores galaxy
 
+    milkyWay.receiveAsteroid(apophis); // galaxy receives asteroid
+    earth.impactByAsteroid(apophis.getName()); // planet impacted by asteroid
 
-    Spaceport::Rocket rocket("Angara A5");
-
-
-    rocket.launchToPlanet(earth);
-    rocket.launchToPlanet(mars);
+    Spaceport::Rocket falcon9("Falcon 9"); // create instance of rockey class, nested wihtin spaceport class
+    falcon9.launchToPlanet(earth); // rocket launches to planet
 
     return 0;
 }
