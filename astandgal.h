0ea01ebe0e922f2f31f919cd2c2e2573eb610b8d
rabs00 <131666461+rabs00@users.noreply.github.com>
2025-02-24 18:15:45 +0300

    Update astandgal.h

--- astandgal.h
+++ astandgal.h
@@ -1,30 +1,6 @@
 #include <string>
 #include <vector>
 
-class CosmicObject {
-public:
-    virtual std::string getName() const = 0;
-};
-
-class Planet : public CosmicObject {
-private:
-    std::string name;
-public:
-    Planet(const std::string& planetName);
-    std::string getName() const override;
-    virtual ~Planet() {}
-    void impactByAsteroid(const std::string& asteroidName) const;
-};
-
-class Spaceship : public CosmicObject {
-private:
-    std::string name;
-public:
-    Spaceship(const std::string& shipName);
-    std::string getName() const override;
-    void exploreGalaxy(const class Galaxy& galaxy) const;
-};
-
 class Asteroid : public CosmicObject {
 private:
     std::string name;
